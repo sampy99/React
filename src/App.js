5077ffae3065aef73ba6f9cae0ae3791027f6acc
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2023-12-13 23:50:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {Fragment,useEffect} from "react";
 const App=()=>{
 
 const [apiId,setApiId] = useState('1')
+const [search,setSearch] = useState(0)
 const [data,setData] = useState({})
 
 console.log(apiId)
@@ -17,6 +18,13 @@ useEffect(()=>{
 console.log('useEffect running')
 if(apiId.length>0 && Number(apiId)>0 && (Number(apiId)<=100)){
 console.log('useEffect if condition');
+apiCall();
+}
+
+return()=>{
+ console.log('cleanup')
+ apiCall();
+ }
 
 const apiCall = async()=>{
 const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${apiId}`)
@@ -29,7 +37,7 @@ setData(data)
 
   apiCall()
  }
-},[apiId])
+},[search])
 
 return (
 <div>
@@ -43,6 +51,8 @@ setApiId(e.target.value)
 
 }}
 placeholder='Enter your ID'/>
+
+<button onClick={()=>setSearch(pre=>pre===0?1:0)} >Search</button>
 
 {data&&(<div>
 <h2>{data.title}</h2>
